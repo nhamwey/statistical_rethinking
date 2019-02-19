c54cc649df865b9fdb4070bb7e3600fcc8d12158
Nicholas Hamwey <34924383+nhamwey@users.noreply.github.com>
2019-02-19 00:13:12 -0500

    finished chapter 4 text code

--- 4_linear_models.R
+++ 4_linear_models.R
@@ -92,3 +92,188 @@ vcov(m4.1)
 
 # extract samples from multidimensional posterior
 post <- extract.samples(m4.1, n = 1e4)
+
+# height covaration with weight
+plot(d2$height ~ d2$weight)
+
+# linear model of height and weight
+data(Howell1)
+d <- Howell1
+d2 <- d[ d$age >= 18 , ]
+m4.3 <- map(
+  alist(
+    height ~ dnorm( mu , sigma ) ,
+    mu <- a + b*weight ,
+    a ~ dnorm( 156 , 100 ) ,
+    b ~ dnorm( 0 , 10 ) ,
+    sigma ~ dunif( 0 , 50 )
+  ) ,
+  data=d2 )
+
+precis(m4.3)
+# center weight
+d2$weight.c <- d2$weight - mean(d2$weight)
+mean(d2$weight.c)
+# refit model with centered weight predictor
+m4.4 <- map(
+  alist(
+    height ~ dnorm( mu , sigma ) ,
+    mu <- a + b*weight.c ,
+    a ~ dnorm( 156 , 100 ) ,
+    b ~ dnorm( 0 , 10 ) ,
+    sigma ~ dunif( 0 , 50 )
+  ) ,
+  data=d2 )
+precis(m4.4, corr = T)
+# plot MAP value
+plot( height ~ weight , data=d2 )
+abline( a=coef(m4.3)["a"] , b=coef(m4.3)["b"] )
+# sample from posterior
+post <- extract.samples(m4.3)
+
+# start with 10 samples
+N <- 100
+dN <- d2[ 1:N , ]
+mN <- map(
+  alist(
+    height ~ dnorm( mu , sigma ) ,
+    mu <- a + b*weight ,
+    a ~ dnorm( 178 , 100 ) ,
+    b ~ dnorm( 0 , 10 ) ,
+    sigma ~ dunif( 0 , 50 )
+  ) , data=dN )
+
+## R code 4.49
+# extract 20 samples from the posterior
+post <- extract.samples( mN , n=1000 )
+
+# display raw data and sample size
+plot( dN$weight , dN$height ,
+      xlim=range(d2$weight) , ylim=range(d2$height) ,
+      col=rangi2 , xlab="weight" , ylab="height" )
+mtext(concat("N = ",N))
+
+# plot the lines, with transparency
+for ( i in 1:20 )
+  abline( a=post$a[i] , b=post$b[i] , col=col.alpha("black",0.3) )
+
+mu_at_50 <- post$a + post$b * 50
+dens(mu_at_50)
+HPDI(mu_at_50, prob = 0.89)
+
+# use link to compute mu
+# for each sample from posterior
+# and for each weight in weight.seq
+weight.seq <- seq( from=25 , to=70 , by=1 )
+mu <- link( m4.3 , data=data.frame(weight=weight.seq) )
+str(mu)
+
+## R code 4.55
+# use type="n" to hide raw data
+plot( height ~ weight , d2 , type="n" )
+
+# loop over samples and plot each mu value
+for ( i in 1:100 )
+  points( weight.seq , mu[i,] , pch=16 , col=col.alpha(rangi2,0.1) )
+
+## R code 4.56
+# summarize the distribution of mu
+mu.mean <- apply( mu , 2 , mean )
+mu.HPDI <- apply( mu , 2 , HPDI , prob=0.89 )
+
+sim.height <- sim( m4.3 , data=list(weight=weight.seq) )
+str(sim.height)
+
+height.PI <- apply( sim.height , 2 , PI , prob=0.89 )
+
+## R code 4.61
+# plot raw data
+plot( height ~ weight , d2 , col=col.alpha(rangi2,0.5) )
+
+# draw MAP line
+lines( weight.seq , mu.mean )
+
+# draw HPDI region for line
+shade( mu.HPDI , weight.seq )
+
+# draw PI region for simulated heights
+shade( height.PI , weight.seq )
+
+sim.height <- sim( m4.3 , data=list(weight=weight.seq) , n=1e4 )
+height.PI <- apply( sim.height , 2 , PI , prob=0.89 )
+
+## R code 4.63
+post <- extract.samples(m4.3)
+weight.seq <- 25:70
+sim.height <- sapply( weight.seq , function(weight)
+  rnorm(
+    n=nrow(post) ,
+    mean=post$a + post$b*weight ,
+    sd=post$sigma ) )
+height.PI <- apply( sim.height , 2 , PI , prob=0.89 )
+
+## R code 4.64
+library(rethinking)
+data(Howell1)
+d <- Howell1
+str(d)
+
+## R code 4.65
+d$weight.s <- ( d$weight - mean(d$weight) )/sd(d$weight)
+
+## R code 4.66
+d$weight.s2 <- d$weight.s^2
+m4.5 <- map(
+  alist(
+    height ~ dnorm( mu , sigma ) ,
+    mu <- a + b1*weight.s + b2*weight.s2 ,
+    a ~ dnorm( 178 , 100 ) ,
+    b1 ~ dnorm( 0 , 10 ) ,
+    b2 ~ dnorm( 0 , 10 ) ,
+    sigma ~ dunif( 0 , 50 )
+  ) ,
+  data=d )
+
+## R code 4.67
+precis( m4.5 )
+
+## R code 4.68
+weight.seq <- seq( from=-2.2 , to=2 , length.out=30 )
+pred_dat <- list( weight.s=weight.seq , weight.s2=weight.seq^2 )
+mu <- link( m4.5 , data=pred_dat )
+mu.mean <- apply( mu , 2 , mean )
+mu.PI <- apply( mu , 2 , PI , prob=0.89 )
+sim.height <- sim( m4.5 , data=pred_dat )
+height.PI <- apply( sim.height , 2 , PI , prob=0.89 )
+
+## R code 4.69
+plot( height ~ weight.s , d , col=col.alpha(rangi2,0.5) )
+lines( weight.seq , mu.mean )
+shade( mu.PI , weight.seq )
+shade( height.PI , weight.seq )
+
+## R code 4.70
+d$weight.s3 <- d$weight.s^3
+m4.6 <- map(
+  alist(
+    height ~ dnorm( mu , sigma ) ,
+    mu <- a + b1*weight.s + b2*weight.s2 + b3*weight.s3 ,
+    a ~ dnorm( 178 , 100 ) ,
+    b1 ~ dnorm( 0 , 10 ) ,
+    b2 ~ dnorm( 0 , 10 ) ,
+    b3 ~ dnorm( 0 , 10 ) ,
+    sigma ~ dunif( 0 , 50 )
+  ) ,
+  data=d )
+
+## R code 4.71
+plot( height ~ weight.s , d , col=col.alpha(rangi2,0.5) , xaxt="n" )
+
+## transform data from z-scores back to original scale
+at <- c(-2,-1,0,1,2)
+labels <- at*sd(d$weight) + mean(d$weight)
+axis( side=1 , at=at , labels=round(labels,1) )
+
+## R code 4.73
+plot( height ~ weight , data=Howell1 ,
+      col=col.alpha(rangi2,0.4) )
